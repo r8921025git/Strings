47370b73175a09e399c49b300096d9ee0c1eb210
r8921025git <r8921025+git@yahoo.com.tw>
2014-12-01 10:13:27 -0500

    Update Palindromicity.cpp

--- Palindromicity.cpp
+++ Palindromicity.cpp
@@ -10,15 +10,45 @@ using std::cout;
 using std::endl;
 using std::string;
 
-// @include
+char ToLower(const char &c) {
+    char out = c;
+    if (c>='A' && c<='Z') {
+        out -= ('A'-'a');
+    }
+    return out;
+}
+
+
+bool IsAlphaNumeric(const char &c) {
+    if ((c>='0'&& c<='9') || (c>='a' && c<='z' || (c>='A'&&c<='Z')))
+        return true;
+    else
+        return false;
+}
+
 bool IsPalindrome(const string& s) {
     string rs = s;
     std::reverse(rs.begin(),rs.end());
     cout<<"s="<<s<<endl;
     cout<<"rs = "<<rs<<endl;
+    int k=0;
+    for (int i=0; i<s.size(); ++i) {
+        if (!IsAlphaNumeric(s[i]))
+            continue;
+        while (!IsAlphaNumeric(rs[k])) {
+            k++;
+            if (k>=rs.size())
+                break;
+        }
+       // cout<<"s[i]="<<s[i]<<", rs[k]="<<rs[k]<<endl;
+        if (ToLower(s[i]) != ToLower(rs[k])) {
+            return false;
+        }
+        k++;
+    }
   return true;
 }
-// @exclude
+
 
 int main(int argc, char** argv) {
   assert(IsPalindrome("A man, a plan, a canal: Panama"));
