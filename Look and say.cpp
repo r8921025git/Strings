0b5280145fbaf53d38272bab20b5e4693379669b
r8921025git <r8921025+git@yahoo.com.tw>
2014-12-01 20:46:08 -0500

    Update Look and say.cpp

--- Look and say.cpp
+++ Look and say.cpp
@@ -18,12 +18,43 @@ using std::to_string;
 using std::uniform_int_distribution;
 
 
-// @include
-string LookAndSay(int n) {
 
+string LookAndSay(int n) {
+    if (n==0) {
+        return "";
+    }
+    if (n==1) {
+        return "1";
+    }
+    if (n==2) {
+        return "11";
+    }
+    string prev_res =LookAndSay(n-1);
+    string new_res = "";
+    
+    int count = 1;
+    for (int i=1; i<prev_res.size(); ++i) {
+        char c = prev_res[i];
+        char d = prev_res[i-1];
+        if (c==d) {
+            count++;
+        }
+        else {
+            string str_count = std::to_string(count);
+            new_res += str_count;
+            new_res += string(1,d);
+        }
+    }
+    string str_count = std::to_string(count);
+    new_res += str_count;
+    char d = prev_res.back();
+    new_res += string(1,d);
+    
+    cout<<"my result: "<<new_res<<endl;
+    return new_res;
 }
 
-// @exclude
+
 
 int main(int argc, char** argv) {
   default_random_engine gen((random_device())());
@@ -31,7 +62,7 @@ int main(int argc, char** argv) {
   if (argc == 2) {
     n = stoi(argv[1]);
   } else {
-    uniform_int_distribution<int> dis(1, 50);
+    uniform_int_distribution<int> dis(1, 6);
     n = dis(gen);
   }
   assert(!LookAndSay(1).compare("1"));
